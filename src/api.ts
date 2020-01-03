c08dbba95b017d57c07e8f92ea02d1dfa34b9c9c
konstantin.yemelianov <konstantin.yemelianov@uberall.com>
2020-01-03 14:49:41 +0200

    Add file upload route

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,89 @@
-import express from "express";
+import express, { Application } from "express";
+import multer from 'multer';
+import path from 'path';
 
-const router = express.Router();
+const limits = {
+	fileSize: 10e6, // 10 Mb
+	files: 10,
+};
 
-router.get('/ping', (req, res) => res.send('API pong\n'));
+const API = (app: Application) => {
+	const router = express.Router();
+	const DATA_FOLDER = app.get("DATA_FOLDER");
 
-export default router;
+	const storage = multer.diskStorage({
+		destination: (req, file, cb) => {
+			cb(null, DATA_FOLDER);
+		},
+
+		// By default, multer removes file extensions so let's add them back
+		filename: (req, file, cb) => {
+			cb(null, 'file_' + Date.now() + path.extname(file.originalname));
+		}
+	});
+
+	router.get(
+		'/ping',
+		(req, res) => res.send('API pong\n')
+	);
+
+	router.post(
+		'/file',
+		(req, res) => {
+			let upload = multer(
+				{
+					storage: storage,
+					limits,
+				}).single('data');
+
+			upload(req, res, err => {
+				// req.file contains information of uploaded file
+				// req.body contains information of text fields, if there were any
+				if (!req.file) {
+					return res.send('Please select an image to upload');
+				}
+				else if (err) {
+					return res.send(err);
+				}
+
+				// Display uploaded image for user validation
+				res.send(req.file.filename);
+			});
+		}
+	);
+
+	router.post(
+		'/files',
+		(req, res) => {
+			let upload = multer(
+				{
+					storage: storage,
+					limits,
+				}).array('data', limits.files);
+
+			upload(req, res, err => {
+				// req.file contains information of uploaded file
+				// req.body contains information of text fields, if there were any
+				if (req.files.length === 0) {
+					return res.send('Please select an image to upload');
+				}
+				else if (err) {
+					return res.send(err);
+				}
+				// const uploadedNames = [];
+				// for (let index = 0, len = req.files.length; index < len; ++index) {
+				// 	uploadedNames.push(req.files[index].filename);
+				// }
+
+				// @ts-ignore
+				const uploadedNames = req.files.map(file => file.filename);
+				// Display uploaded image for user validation
+				res.send(uploadedNames);
+			});
+		}
+	);
+
+	return router;
+};
+
+export default API;
--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,22 @@
-import express, {NextFunction, Request, Response} from "express";
+import express, {NextFunction, Request, Response, Application} from "express";
 import path from "path";
 import { v1 as uuid } from 'uuid';
 
 import API from './api';
 
 // Create Express server
-const app = express();
+const app: Application = express();
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 8010);
 const FILE_SERVER_API_KEY = process.env.FILE_SERVER_API_KEY || uuid();
 app.set("FILE_SERVER_API_KEY", FILE_SERVER_API_KEY);
+const DATA_FOLDER = path.resolve(__dirname, "..", "data");
+app.set("DATA_FOLDER", DATA_FOLDER);
 
 app.use(
 	'/data',
-	express.static(path.resolve(__dirname, "..", "data"), { maxAge: 31557600000 })
+	express.static(DATA_FOLDER, { maxAge: 31557600000 })
 );
 
 app.get('/ping', (req, res) => res.send('pong'));
@@ -27,6 +29,6 @@ const check_FILE_SERVER_API_KEY = (req: Request, res: Response, next: NextFuncti
 	next();
 };
 
-app.use('/api', check_FILE_SERVER_API_KEY, API);
+app.use('/api', check_FILE_SERVER_API_KEY, API(app));
 
 export default app;
